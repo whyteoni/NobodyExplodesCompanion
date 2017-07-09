a0080c59cd94973e0bf590b287618e52c5e1f924
Nathaniel Miller <whyte.oni@gmail.com>
2017-07-09 10:12:28 -0400

    Started to convert to grids

--- NoBodyExplodesCompanion.py
+++ NoBodyExplodesCompanion.py
@@ -3,25 +3,26 @@ import Tkinter as tk
 
 root = tk.Tk()
 
-top_fr = tk.Frame(root)
-top_fr.pack()
-intro = tk.Label(top_fr, text="Nobody Explodes Companion", font="Times 22 bold")
-intro.pack(side='left')
-bt_killit = tk.Button(
-    top_fr, 
+# top_fr = tk.Frame(root)
+# top_fr.pack(side='top')
+intro = tk.Label(
+    text="Nobody Explodes Companion", 
+    font="Times 22 bold", 
+    justify='left').grid(row=0, column=0)
+# intro.pack(side='left')
+bt_killit = tk.Button( 
     text="Make It Stop", 
     fg="red", 
-    bg="black", 
     font="Helvetica 12 bold",
     command=root.destroy)
-bt_killit.pack(side='right')
+bt_killit.grid(row=0, column=2)
 
-bottom_fr = tk.Frame(root)
-bottom_fr.pack(side='bottom')
-bt_wires = tk.Button(bottom_fr, text="Wires", font="Helvetica 16", width=10)
-bt_wires.pack(side='right')
-bt_symbols = tk.Button(bottom_fr, text="Symbols", font="Helvetica 16", width=10)
-bt_symbols.pack(side='right')
+# rightside_fr = tk.Frame(root)
+# rightside_fr.pack(side='bottom')
+# bt_wires = tk.Button(rightside_fr, text="Wires", width=10)
+# bt_wires.pack(side='right')
+# bt_symbols = tk.Button(rightside_fr, text="Symbols", width=10)
+# bt_symbols.pack(side='right')
 
 
 root.mainloop()
